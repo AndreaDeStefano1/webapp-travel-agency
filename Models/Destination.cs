bdf776dbd677ca7731c6bc3d17d1c6600522ca37
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-10-20 11:12:25 +0200

    Aggiungere i file di progetto.

---
+++ Models/Destination.cs
@@ -0,0 +1,14 @@
+﻿namespace webapp_travel_agency.Models
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public Destination()
+        {
+
+        }
+    }
+}
---
+++ Models/PacchettoViaggio.cs
@@ -0,0 +1,11 @@
+﻿namespace webapp_travel_agency.Models
+{
+    public class PacchettoViaggio
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Duration { get; set; }
+        public Destination Destination 
+    }
+}
